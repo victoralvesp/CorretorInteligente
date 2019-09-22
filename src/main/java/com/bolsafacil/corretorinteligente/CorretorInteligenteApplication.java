97648fd76ace1228fcca7754c3a22253d4815717
Victor Alves <vpalves1@gmail.com>
2019-09-21 22:22:02 -0300

    Adicionando arquivos inicializados do projeto Spring Initializr
    Dependencias adicionadas:
    	- spring-boot-starter-web
    	- spring-boot-data-jpa
    	- spring-boot-starter-test
    	- postgresql

---
+++ src/main/java/com/bolsafacil/corretorinteligente/CorretorInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.bolsafacil.corretorinteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorretorInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorretorInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolsafacil/corretorinteligente/CorretorInteligenteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bolsafacil.corretorinteligente;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorretorInteligenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
